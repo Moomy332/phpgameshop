d57f2bdd27cb157cf255fd0ee1b0615969520889
Moomy332 <138703060+Moomy332@users.noreply.github.com>
2023-07-06 00:43:16 +0200

    semin

--- browse.php
+++ browse.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./stilovi/browse.css">
   <link rel="icon" href="./pictures/favicon-logo.ico">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Kdam+Thmor+Pro:wght@400&amp;display=swap"data-tag="font" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Kdam+Thmor+Pro:wght@400&amp;display=swap" data-tag="font" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://kit.fontawesome.com/a620d9a1da.js" crossorigin="anonymous"></script>
@@ -15,101 +15,106 @@
 </head>
 
 <body>
-<?php
+  <?php
   include_once 'header.php'
   ?>
   <style>
     #browse-boja {
       color: rgb(51, 255, 0);
     }
-    #browse-boja:hover{
-    color: rgb(21, 128, 0);
+
+    #browse-boja:hover {
+      color: rgb(21, 128, 0);
     }
-    
-    #footer{
-        position: relative;
+
+    #footer {
+      position: relative;
     }
   </style>
-  <div id="bg" >
+  <div id="bg">
     <div id="messageAdd"></div>
-      <div id="browse-wrapper">
-        <?php
-        include './includes/dbh.inc.php';
-        $stmt= $conn->prepare("SELECT * FROM games"); 
-        $stmt->execute();
-        $result=$stmt->get_result();
-        while($row=$result->fetch_assoc()):
-        ?>
-        <div class="kockice-wrapper" >
-            <div class="kockice" >
-              <img src="<?=$row['gamesImage']?>" height="280"> 
-              <div style="padding: 5px; font-size: 25px;">
-              <h5 style="color: red; text-align:center"><?=$row['gamesName']?></h5>
-              <h5 style="color: black; text-align:center"><?=number_format($row['gamesPrice']) ?> KM</h5>
-              </div>
-              <div id="donji-addtocart">
-                <form class="form-submit">
-                  <input type="hidden" class="pid" value="<?= $row['gamesId']?>">
-                  <input type="hidden" class="pname" value="<?= $row['gamesName']?>">
-                  <input type="hidden" class="pprice" value="<?= $row['gamesPrice']?>">
-                  <input type="hidden" class="pimage" value="<?= $row['gamesImage']?>">
-                  <input type="hidden" class="pcode" value="<?= $row['gamesCode']?>">
-                  <button class='browseadd-but'>Add to cart</button>
-                </form>
-                
-              </div>
+    <div id="browse-wrapper">
+      <?php
+      include './includes/dbh.inc.php';
+      $stmt = $conn->prepare("SELECT * FROM games");
+      $stmt->execute();
+      $result = $stmt->get_result();
+      while ($row = $result->fetch_assoc()) :
+      ?>
+        <div class="kockice-wrapper">
+          <div class="kockice">
+            <img src="<?= $row['gamesImage'] ?>" height="280">
+            <div style=" font-size: 25px;">
+              <h5 style="color: red; text-align:center"><?= $row['gamesName'] ?></h5>
+              <h5 style="color: black; text-align:center"><?= number_format($row['gamesPrice']) ?> KM</h5>
             </div>
+            <div id="donji-addtocart">
+              <form class="form-submit">
+                <input type="hidden" class="pid" value="<?= $row['gamesId'] ?>">
+                <input type="hidden" class="pname" value="<?= $row['gamesName'] ?>">
+                <input type="hidden" class="pprice" value="<?= $row['gamesPrice'] ?>">
+                <input type="hidden" class="pimage" value="<?= $row['gamesImage'] ?>">
+                <input type="hidden" class="pcode" value="<?= $row['gamesCode'] ?>">
+                <button class='browseadd-but'>Add to cart</button>
+              </form>
+
+            </div>
+          </div>
         </div>
-        <?php 
-        endwhile;
-        ?>
-      </div>
+      <?php
+      endwhile;
+      ?>
+    </div>
   </div>
 
   <?php
-include_once 'footer.php'
-?>
+  include_once 'footer.php'
+  ?>
 
-<script type="text/javascript">
-$(document).ready(function(){
-  $(".browseadd-but").click(function(e){
-    e.preventDefault(); //pauzira refreshovanje stranice kad se klikne add to cart
-    var $form = $(this).closest(".form-submit");  //trazi formu, a ispod uzima vrijednosti trazenih klasa  
-    var pid = $form.find(".pid").val();
-    var pname = $form.find(".pname").val();
-    var pprice = $form.find(".pprice").val();
-    var pimage = $form.find(".pimage").val();
-    var pcode = $form.find(".pcode").val();
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $(".browseadd-but").click(function(e) {
+        e.preventDefault(); //pauzira refreshovanje stranice kad se klikne add to cart
+        var $form = $(this).closest(".form-submit"); //trazi formu, a ispod uzima vrijednosti trazenih klasa  
+        var pid = $form.find(".pid").val();
+        var pname = $form.find(".pname").val();
+        var pprice = $form.find(".pprice").val();
+        var pimage = $form.find(".pimage").val();
+        var pcode = $form.find(".pcode").val();
 
-    $.ajax({    //salje bazi podatke
-      url: './includes/action.php',
-      method:'post',
-      data: {pid:pid,
-            pname:pname,
-            pprice:pprice,
-            pimage:pimage,
-            pcode:pcode},
-      success:function(response){
-        $("#messageAdd").html(response);
-        window.scrollTo(0,0);
-        load_cart_item_number();
+        $.ajax({ //salje bazi podatke
+          url: './includes/action.php',
+          method: 'post',
+          data: {
+            pid: pid,
+            pname: pname,
+            pprice: pprice,
+            pimage: pimage,
+            pcode: pcode
+          },
+          success: function(response) {
+            $("#messageAdd").html(response);
+            window.scrollTo(0, 0);
+            load_cart_item_number();
+          }
+        });
+      });
+      load_cart_item_number();
+
+      function load_cart_item_number() { //automatsko brojanje broja igara u korpi
+        $.ajax({
+          url: './includes/action.php',
+          method: 'get',
+          data: {
+            cartItem: "cart_item"
+          },
+          success: function(response) {
+            $("#cart-item").html(response);
+          }
+        })
       }
     });
-  });
-  load_cart_item_number();
-
-  function load_cart_item_number(){  //automatsko brojanje broja igara u korpi
-    $.ajax({
-      url: './includes/action.php',
-      method:'get',
-      data:{cartItem:"cart_item"},
-      success:function(response){
-        $("#cart-item").html(response);
-      }
-    })
-  }
-});
-</script>
+  </script>
 
 </body>
 
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <link rel="icon" href="./pictures/favicon-logo.ico">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="./stilovi/index.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Kdam+Thmor+Pro:wght@400&amp;display=swap"data-tag="font" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Kdam+Thmor+Pro:wght@400&amp;display=swap" data-tag="font" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://kit.fontawesome.com/a620d9a1da.js" crossorigin="anonymous"></script>
@@ -16,16 +16,17 @@
 </head>
 
 <body>
-<?php
+  <?php
   include_once 'header.php'
   ?>
   <style>
     #home-boja {
       color: rgb(51, 255, 0);
-}
-#home-boja:hover{
-  color: rgb(21, 128, 0);
-}
+    }
+
+    #home-boja:hover {
+      color: rgb(21, 128, 0);
+    }
   </style>
   <div id="bg">
     <div class="w3-content w3-section" style="max-width:1450px">
@@ -38,43 +39,46 @@
       <img class="mySlides w3-animate-left" src="./pictures/posteri/metro.jpg" style="width: 100%;">
       <img class="mySlides w3-animate-left" src="./pictures/posteri/metroid.jpg" style="width: 100%;">
     </div>
-  <div id="best-selling">
-      <h1 >Best selling</h1>
-    <div id="gallery-wrapper">
-    <div class="image">
-      <img src="./pictures/home/home1.jpg" width="300" height="200">
-      <p>20KM</p>
-      <div class="desc">Lies of P</div>
-      <button class="addtocart"><a href="">Add to cart</a></button>
+    <div id="best-selling">
+      <h1>Best selling</h1>
+      <div id="browse-wrapper">
+        <?php
+        include './includes/dbh.inc.php';
+        $stmt= $conn->prepare("SELECT * FROM games WHERE gamesId IN(9,13,6,2)"); 
+        $stmt->execute();
+        $result=$stmt->get_result();
+        while($row=$result->fetch_assoc()):
+        ?>
+        <div class="kockice-wrapper" >
+            <div class="kockice" >
+              <img src="<?=$row['gamesImage']?>" height="280"> 
+              <div style="padding: 5px; font-size: 25px;">
+              <h5 style="color: red; text-align:center"><?=$row['gamesName']?></h5>
+              <h5 style="color: black; text-align:center"><?=number_format($row['gamesPrice']) ?> KM</h5>
+              </div>
+              <div id="donji-addtocart">
+                <form class="form-submit">
+                  <input type="hidden" class="pid" value="<?= $row['gamesId']?>">
+                  <input type="hidden" class="pname" value="<?= $row['gamesName']?>">
+                  <input type="hidden" class="pprice" value="<?= $row['gamesPrice']?>">
+                  <input type="hidden" class="pimage" value="<?= $row['gamesImage']?>">
+                  <input type="hidden" class="pcode" value="<?= $row['gamesCode']?>">
+                  <button class='browseadd-but'>Add to cart</button>
+                </form>
+                
+              </div>
+            </div>
+        </div>
+        <?php 
+        endwhile;
+        ?>
+      </div>
     </div>
-
-    <div class="image">
-      <img src="./pictures/home/home2.jpg" width="300" height="200">
-      <p >49KM</p>
-      <div class="desc">Lords of the Fallen</div>
-      <button class="addtocart"><a href="">Add to cart</a></button>
-    </div>
-
-    <div class="image">
-      <img src="./pictures/home/home3.jpg" width="300" height="200">
-      <p>15KM</p>
-      <div class="desc">The Crew</div>
-      <button class="addtocart"><a href="">Add to cart</a></button>
-    </div>
-
-    <div class="image">
-      <img src="./pictures/home/home4.jpg" width="300" height="200">
-      <p>33KM</p>
-      <div class="desc">F1: 2023</div>
-      <button class="addtocart"><a href="">Add to cart</a></button>
-    </div>
-    </div>
-  </div>
 
     <div id="free-games">
-      
+
       <div id="free-wrapper">
-      <h1>Free Games</h1>
+        <h1>Free Games</h1>
         <img id="thumb1" src="./pictures/home/free-1.jpg">
         <img id="thumb2" src="./pictures/home/free-2.jpg">
       </div>
@@ -84,8 +88,8 @@
     </div>
   </div>
   <?php
-include_once 'footer.php'
-?>
+  include_once 'footer.php'
+  ?>
   <script>
     var myIndex = 0;
     carousel();
@@ -97,50 +101,56 @@ include_once 'footer.php'
         x[i].style.display = "none";
       }
       myIndex++;
-      if (myIndex > x.length) { myIndex = 1 }
+      if (myIndex > x.length) {
+        myIndex = 1
+      }
       x[myIndex - 1].style.display = "block";
       setTimeout(carousel, 5000);
     }
   </script>
   <script type="text/javascript">
-$(document).ready(function(){
-  $(".browseadd-but").click(function(e){
-    e.preventDefault(); //pauzira refreshovanje stranice kad se klikne add to cart
-    var $form = $(this).closest(".form-submit");  //trazi formu, a ispod uzima vrijednosti trazenih klasa  
-    var pid = $form.find(".pid").val();
-    var pname = $form.find(".pname").val();
-    var pprice = $form.find(".pprice").val();
-    var pimage = $form.find(".pimage").val();
-    var pcode = $form.find(".pcode").val();
+    $(document).ready(function() {
+      $(".browseadd-but").click(function(e) {
+        e.preventDefault(); //pauzira refreshovanje stranice kad se klikne add to cart
+        var $form = $(this).closest(".form-submit"); //trazi formu, a ispod uzima vrijednosti trazenih klasa  
+        var pid = $form.find(".pid").val();
+        var pname = $form.find(".pname").val();
+        var pprice = $form.find(".pprice").val();
+        var pimage = $form.find(".pimage").val();
+        var pcode = $form.find(".pcode").val();
 
-    $.ajax({    //salje bazi podatke
-      url: './includes/action.php',
-      method:'post',
-      data: {pid:pid,
-            pname:pname,
-            pprice:pprice,
-            pimage:pimage,
-            pcode:pcode},
-      success:function(response){
-        $("#messageAdd").html(response);
-        load_cart_item_number();
+        $.ajax({ //salje bazi podatke
+          url: './includes/action.php',
+          method: 'post',
+          data: {
+            pid: pid,
+            pname: pname,
+            pprice: pprice,
+            pimage: pimage,
+            pcode: pcode
+          },
+          success: function(response) {
+            $("#messageAdd").html(response);
+            load_cart_item_number();
+          }
+        });
+      });
+      load_cart_item_number();
+
+      function load_cart_item_number() {
+        $.ajax({
+          url: './includes/action.php',
+          method: 'get',
+          data: {
+            cartItem: "cart_item"
+          },
+          success: function(response) {
+            $("#cart-item").html(response);
+          }
+        })
       }
     });
-  });
-  load_cart_item_number();
-
-  function load_cart_item_number(){
-    $.ajax({
-      url: './includes/action.php',
-      method:'get',
-      data:{cartItem:"cart_item"},
-      success:function(response){
-        $("#cart-item").html(response);
-      }
-    })
-  }
-});
-</script>
+  </script>
 </body>
 
 </html>
